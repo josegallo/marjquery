f2628a35f78c0553e3f6e172b191d80df609597f
josegallo <josegallo@gmail.com>
2019-04-25 15:24:20 +0200

    scrolling elmenents at home

---
+++ home.js
@@ -0,0 +1,35 @@
+<script>
+(function ($) {
+	$(document).ready(function () {
+        $(".arrow-wow").click(function() {
+                var heightA1 = $("div.begin-section").height();
+                var heightE1 = parseInt(heightA1);
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(".begin-section").offset().top - heightE1
+            }, 2000);
+        });
+
+        $(".arrow-clientes").click(function() {
+                var heightA2 = $(".section-piensas-que").height();
+                var heightE2 = parseInt(heightA2);
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(".arrow-piensas-que").offset().top - 170
+            }, 2000);
+        });
+
+        $(".arrow-piensas-que").click(function() {
+                var heightA3 = $(".section-arrow-somos-devotos").height();
+                var heightE3 = parseInt(heightA3);
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(".arrow-somos-devotos").offset().top - heightE3
+            }, 2000);
+        });
+
+        $(".arrow-somos-devotos").click(function() {
+            $("html, body").animate({ scrollTop: $(document).height() }, 3000);
+        });
+    });
+})(jQuery);
+
+</script>
+
