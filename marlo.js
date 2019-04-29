3eded2d5fdfbb75763b987e5cc6a8e63c74e94ef
josegallo <josegallo@gmail.com>
2019-04-29 20:04:57 +0200

    depurate

--- marlo.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-	$(document).ready(function () {
-				 var shareIcons = document.getElementById('div#share-holder');
-				 shareIcons.classList.add('active');
-				 console.log ('icons');
-				 $("div#share-holder").css("display","block");
-				 $("section#comments").css("display","block");
-  });
-})(jQuery);
